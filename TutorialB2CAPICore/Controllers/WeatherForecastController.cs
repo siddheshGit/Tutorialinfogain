edb4f0091710ffb00b5db82b418593625a3d5d51
siddhesh gawas <sid.gawas@live.in>
2019-11-01 12:26:38 +0530

    new commit

---
+++ TutorialB2CAPICore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TutorialB2CAPI.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetWeather")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetClaims")]
+        public string GetCalims()
+        {
+            string displayName = User.FindFirst(User.Identities.First().NameClaimType).ToString();
+
+            displayName = !String.IsNullOrEmpty(displayName)  ? displayName : String.Empty;
+
+            return displayName;
+        }
+    }
+}
---
+++ TutorialB2CAppCore/AzureAdB2COptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorialB2CApp
+{
+    public class AzureAdB2COptions
+    {
+
+        public const string SelecteuserFlowKey = "UserFlow";
+        //public const string PolicyAuthenticationProperty = "Policy";
+
+
+
+
+        //public AzureAdB2COptions()
+        //{
+        //    AzureAdB2CInstance= "https://login.microsoftonline.com/tfp";
+        //}
+
+        public string ClientId { get; set; }
+        public string AzureAdB2CInstance { get; set; }
+        public string Tenant { get; set; }
+       // public string SignUpSignInPolicyId { get; set; }
+        public string SignUpSignInUserFlowId { get; set; }
+
+        //public string SignInPolicyId { get; set; }
+        //public string SignInUserFlowId { get; set; }
+        //public string SignUpPolicyId { get; set; }
+        //public string ResetPasswordPolicyId { get; set; }
+        public string ResetPasswordUserFlowId { get; set; }
+        public string EditProfilePolicyId { get; set; }
+        public string EditProfileUserFlowId { get; set; }
+        public string RedirectUri { get; set; }
+
+        public string DefaultPolicy => SignUpSignInUserFlowId;
+        public string Authority => $"{AzureAdB2CInstance}/{Tenant}/{DefaultPolicy}/v2.0";
+
+        public string ClientSecret { get; set; }
+        public string ApiUrl { get; set; }
+        public string ApiScopes { get; set; }
+
+    }
+}
